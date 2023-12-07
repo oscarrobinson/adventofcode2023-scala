121f899cda1e478462d8660c4dbcb1cfbbd04c69
Oscar Robinson <oscar.w.robinson@gmail.com>
2023-12-07 08:54:49 +0000

    Day 7 progress

---
+++ src/main/scala/com/oscarrobinson/adventofcode/seventh/DaySeven.scala
@@ -0,0 +1,54 @@
+package com.oscarrobinson.adventofcode.seventh
+
+import com.oscarrobinson.adventofcode.utils.Utils
+
+case class Hand(cards: String, bid: Long) {
+  val cardsScore = List('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').reverse.zipWithIndex.toMap.view.mapValues(_ + 2).toMap
+
+  val groupedHand = {
+    cards
+      .toList
+      .groupBy((char: Char) => char)
+      .values.toList
+      .sortBy(groupList => {
+        // Ensure sorted by largest group of cards first, then sort groups of same size by how high scoring the card is
+        groupList.size * 100 + cardsScore(groupList.head)
+      })
+      .reverse
+  }
+
+  def typeRank(): Int = {
+    groupedHand match
+      // five of a kind
+      case _ :: Nil => 6
+      // four of a kind
+      case group :: _ if group.size == 4 => 5
+      // full house
+      case firstGroup :: secondGroup :: Nil if firstGroup.size == 3 && secondGroup.size == 2 => 4
+      // three of a kind
+      case firstGroup :: _ if firstGroup.size == 3 => 3
+      // two pair
+      case firstGroup :: secondGroup :: _ if firstGroup.size == 2 && secondGroup.size == 2 => 2
+      // one pair
+      case firstGroup :: _ if firstGroup.size == 2 => 1
+      // high card
+      case _ => 0
+  }
+  def cardsRank(): Int = groupedHand.map(_.head).flatMap(cardsScore.get).map(num => "%02d".format(num)).mkString.toInt
+  
+  def totalRank(): Int = typeRank() * 1
+}
+
+def daySevenPartOne(filename: String): Long = {
+  0L
+}
+
+def daySevenPartTwo(filename: String): Long = {
+  0L
+}
+
+@main
+def main(): Unit = {
+  println(s"Part 1: ${daySevenPartOne("inputs/7/input.txt")}")
+  println(s"Part 2: ${daySevenPartTwo("inputs/7/input.txt")}")
+}
---
+++ src/test/scala/com/oscarrobinson/adventofcode/seventh/DaySeven.scala
@@ -0,0 +1,75 @@
+package com.oscarrobinson.adventofcode.seventh
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class DaySeven extends AnyWordSpec with Matchers {
+
+  "Hand.typeRank" should {
+
+    "return correct rank for high card" in {
+      Hand("23456", 100).typeRank() shouldEqual 0
+    }
+
+    "return correct rank for one pair" in {
+      Hand("32T3K", 100).typeRank() shouldEqual 1
+    }
+
+    "return correct rank for two pair" in {
+      Hand("KK677", 100).typeRank() shouldEqual 2
+    }
+
+    "return correct rank for three of a kind" in {
+      Hand("T55J5", 100).typeRank() shouldEqual 3
+    }
+
+    "return correct rank for full house" in {
+      Hand("23332", 100).typeRank() shouldEqual 4
+    }
+
+    "return correct rank for four of a kind" in {
+      Hand("AA8AA", 100).typeRank() shouldEqual 5
+    }
+
+    "return correct rank for five of a kind" in {
+      Hand("AAAAA", 100).typeRank() shouldEqual 6
+    }
+  }
+
+  "Hand.cardsRank" should {
+    "return correct rank for a high card hand" in {
+      Hand("A2345", 100).cardsRank() shouldEqual 1405040302
+    }
+
+    "return correct rank for a five of a kind hand" in {
+      Hand("AAAAA", 100).cardsRank() shouldEqual 14
+    }
+
+    "return correct rank for a two pair hand" in {
+      Hand("22879", 100).cardsRank() shouldEqual 2090807
+    }
+  }
+
+  "Hand.totalRank" should {
+    "return correct rank for a five of a kind hand" in {
+      Hand("A2345", 100).totalRank() shouldEqual 145432
+    }
+
+    "return correct rank for a one pair hand" in {
+      Hand("22345", 100).totalRank() shouldEqual 114543
+    }
+  }
+
+
+  "daySevenPartOne" should {
+    "return correct number" in {
+      daySevenPartOne("inputs/7/test_input.txt") shouldEqual 6440L
+    }
+  }
+
+  "daySevenPartTwo" should {
+    "return correct number" in {
+      daySevenPartTwo("inputs/7/test_input.txt") shouldEqual 1L
+    }
+  }
+}
