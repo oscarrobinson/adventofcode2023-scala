02326b56c4dfb219cbc535f681c6141e4a2cf5c7
Oscar Robinson <oscar.w.robinson@gmail.com>
2023-12-08 20:17:51 +0000

    Day eight part 2

--- src/main/scala/com/oscarrobinson/adventofcode/eighth/DayEight.scala
+++ src/main/scala/com/oscarrobinson/adventofcode/eighth/DayEight.scala
@@ -2,6 +2,57 @@ package com.oscarrobinson.adventofcode.eighth
 
 import com.oscarrobinson.adventofcode.utils.Utils
 
+import scala.annotation.tailrec
+
+
+case class GhostDesertMap(rawDirections: String, mapLines: List[String]) {
+  val directions = rawDirections.toList
+
+  val directionsIterator = LazyList.continually(directions).flatten
+
+  val map = {
+    mapLines.map(line => {
+      val nodeAddr = line.split('=').head.strip()
+      val left = line.split('=').last.split(',').head.filterNot(_ == '(').strip()
+      val right = line.split('=').last.split(',').last.filterNot(_ == ')').strip()
+      (nodeAddr, Map(('L', left), ('R', right)))
+    }).toMap
+  }
+
+  def stepsFromStartingLocationToFinish(startingLocation: String): Long = {
+    var stepsTaken = 0L
+    var currentLocation = startingLocation
+    directionsIterator.takeWhile(_ => currentLocation.last != 'Z').foreach(direction => {
+      currentLocation = map(currentLocation)(direction)
+      stepsTaken += 1
+    })
+    stepsTaken
+  }
+
+  @tailrec
+  private def gcd(a: Long, b: Long): Long = b match {
+    case 0L => a
+    case _ => gcd(b, a % b)
+  }
+
+  private def lcm(a: Long, b: Long): Long = a * (b / gcd(a, b))
+
+  private def lcm(numList: List[Long]): Long = numList match {
+    case Nil => 0L
+    case num :: Nil => num
+    case num :: rest => lcm(num, lcm(rest))
+  }
+
+  def steps: Long = {
+    val startingLocations = map.keys.filter(_.last == 'A').toList
+    // Every path must loop, so first get num steps from start to finish for every starting location
+    val stepsToFinish = startingLocations.map(stepsFromStartingLocationToFinish)
+    // We then need to work out how many steps til all journeys are on the finish simultaneously
+    // This will be the lowest common multiple of the total steps for each individual journey
+    lcm(stepsToFinish)
+  }
+
+}
 
 case class DesertMap(rawDirections: String, mapLines: List[String]) {
   val directions = rawDirections.toList
@@ -39,7 +90,8 @@ def dayEightPartOne(filename: String): Long = {
 
 def dayEightPartTwo(filename: String): Long = {
   val fileLines = Utils.fileLinesAs(filename, _.toList)
-  val desertMap = DesertMap(fileLines.head, fileLines.tail.filter(_.nonEmpty))
+  val desertMap = GhostDesertMap(fileLines.head, fileLines.tail.filter(_.nonEmpty))
+  desertMap.steps
 }
 
 @main
