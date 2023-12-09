1d5727c68f869f021ff3eb5e09bb3b3d59e7ab05
Oscar Robinson <oscar.w.robinson@gmail.com>
2023-12-09 12:45:06 +0000

    day 9 template

---
+++ src/main/scala/com/oscarrobinson/adventofcode/ninth/DayNine.scala
@@ -0,0 +1,17 @@
+package com.oscarrobinson.adventofcode.ninth
+
+import com.oscarrobinson.adventofcode.utils.Utils
+
+def dayNinePartOne(filename: String): Long = {
+  0L
+}
+
+def dayNinePartTwo(filename: String): Long = {
+  0L
+}
+
+@main
+def main(): Unit = {
+  println(s"Part 1: ${dayNinePartOne("inputs/9/input.txt")}")
+  println(s"Part 2: ${dayNinePartTwo("inputs/9/input.txt")}")
+}
---
+++ src/test/scala/com/oscarrobinson/adventofcode/ninth/DayNine.scala
@@ -0,0 +1,18 @@
+package com.oscarrobinson.adventofcode.ninth
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class DayNine extends AnyWordSpec with Matchers {
+  "dayNinePartOne" should {
+    "return correct number" in {
+      dayNinePartOne("inputs/9/test_input.txt") shouldEqual 1L
+    }
+  }
+
+  "daySevenPartTwo" should {
+    "return correct number" in {
+      dayNinePartTwo("inputs/9/test_input.txt") shouldEqual 1L
+    }
+  }
+}
